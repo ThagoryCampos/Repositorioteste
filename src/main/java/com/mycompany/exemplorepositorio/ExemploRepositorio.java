fbd6401990313848b7e94c2cfc8e44be4398e4e4
THAGOR� OLIVEIRA CAMPOS <thagory.8187@aluno.pr.senac.br>
2023-09-05 15:44:42 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/exemplorepositorio/ExemploRepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplorepositorio;
+
+/**
+ *
+ * @author thagory.8187
+ */
+public class ExemploRepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
